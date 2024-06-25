73d6932323846718c0dad0a79b7e6cc57a1e780f
Ahmed-Magdi1 <Ahmed216123@gmail.com>
2024-06-25 20:43:34 +0300

    update

---
+++ myrobot/build/CMakeFiles/Welcom.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Welcom.dir/script/Welcom.cpp.o"
+  "libWelcom.pdb"
+  "libWelcom.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Welcom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
